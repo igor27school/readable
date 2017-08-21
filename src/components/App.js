0de6d36d1767d8e58a5c1d7c95de7ee2a7b9b941
Igor <igor27school@gmail.com>
2017-08-21 13:50:08 -0700

    refactor: move App.js into components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import * as PostsAPI from './utils/PostsAPI';
+import * as PostsAPI from '../utils/PostsAPI';
 
 class App extends Component {
   state = {
